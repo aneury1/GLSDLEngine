0399f5f59986ae6b772f448f9938c633bdbabb13
aneury perez <aneury1@gmail.com>
2019-02-06 17:25:58 -0400

    fixed   VertexModel
    here I already have found what I did to damage the VertexModel. is state main.cpp

--- VertexModel.cpp
+++ VertexModel.cpp
@@ -22,17 +22,19 @@ void VertexModel::unbind()
 
 void VertexModel::addVertices(const vector<GLfloat>& vertices, int dimension)
 {
-     GLuint VBO;
      printf("index counter =%d\n", index_counter);
+     GLuint VBO;
+    
      glGenBuffers(1, &VBO);
      glBindBuffer(GL_ARRAY_BUFFER, VBO);
      glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(vertices[0]), vertices.data(),GL_STATIC_DRAW);
      glVertexAttribPointer(index_counter, dimension ,GL_FLOAT, GL_FALSE, 0, (void *)0);
      glEnableVertexAttribArray(this->index_counter);
-     buffer_list.push_back(VBO);
-     glBindVertexArray(0);  
      glBindBuffer(GL_ARRAY_BUFFER, 0);
+     
+    
      this->index_counter++;
+     buffer_list.push_back(VBO);
 }
 
  
--- main.cpp
+++ main.cpp
@@ -50,34 +50,43 @@ int main(int argc, char *argv[]) {
 
    float my_vertices[]={
     // first triangle
-       -0.120312f, 0.050000f, 0.0f, 1 , 0 ,0,
-      -0.048437f, 0.208333f, 0.0f, 0 , 1 ,0,
-      -0.217187f, 0.195833f, 0.0f, 0 , 0 ,1,
-      
-      0.735937, -0.524999 , 0.0f, 0 , 0 ,1,
-      0.860937, -0.810416 , 0.0f, 0 , 1 ,0,
-       0.584375, -0.829166 , 0.0f, 1 , 0 ,0,
+      //  -0.120312f, 0.050000f, 0.0f, 1 , 0 ,0,
+      // -0.048437f, 0.208333f, 0.0f, 0 , 1 ,0,
+      // -0.217187f, 0.195833f, 0.0f, 0 , 0 ,1,
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+      // 0.735937, -0.524999 , 0.0f, 0 , 0 ,1,
+      // 0.860937, -0.810416 , 0.0f, 0 , 1 ,0,
+      //  0.584375, -0.829166 , 0.0f, 1 , 0 ,0,
 
-      0.800000, 0.056249, 0.0f, 0.78 , 0.34344 ,1,
-       0.181250, -0.787500, 0.0f, 0.898 , 0.787 ,0.7878871,
-      0.929687, 0.627083, 0.0f, 0.56767 , 0.78787 ,1,
+      // 0.800000, 0.056249, 0.0f, 0.78 , 0.34344 ,1,
+      //  0.181250, -0.787500, 0.0f, 0.898 , 0.787 ,0.7878871,
+      // 0.929687, 0.627083, 0.0f, 0.56767 , 0.78787 ,1,
  };
-
+ float colors[]={
+    // first triangle
+      //  -0.120312f, 0.050000f, 0.0f, 1 , 0 ,0,
+      // -0.048437f, 0.208333f, 0.0f, 0 , 1 ,0,
+      // -0.217187f, 0.195833f, 0.0f, 0 , 0 ,1,
+    0.5f, 0.5f, 1.0f,
+     0.5f, 0.5f, 1.0f,
+     1.0f,  0.5f, 1.0f};
    vector <float> test({
       -0.5f, -0.5f, 0.0f,
        0.5f, -0.5f, 0.0f,
        0.0f,  0.5f, 0.0f});
     vector <float> color({
       1.0f, 0.0f, 0.0f,
-      1.0f, 0.0f, 0.0f,
-      1.0f, 0.0f, 0.0f
+      0.0f, 1.0f, 0.0f,
+      0.0f, 0.0f, 1.0f
     });
 
  
 
-  VertexModel model(test,3);
-  model.addVertices(color, 3);
-  
+   VertexModel model(test,3);
+   model.addVertices(color, 3);
+   model.unbind();
   
     GLuint VAO, VBO;
     glGenVertexArrays(1, &VAO);
@@ -85,10 +94,18 @@ int main(int argc, char *argv[]) {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof( my_vertices), my_vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float) , (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float) , (void *)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float) , ((void *)(3*sizeof(float))));
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    GLuint color_vBo;
+    glGenBuffers(1, &color_vBo);
+    glBindBuffer(GL_ARRAY_BUFFER, color_vBo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof( colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float) , (void *)0);
     glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+ 
 
 
 
@@ -100,11 +117,10 @@ int main(int argc, char *argv[]) {
     window->PollEvent();
     program->Use();  
 
- model.bind();
-glDrawArrays(GL_TRIANGLES,0, 3);
-    glBindVertexArray(VAO);
-    
-     glDrawArrays(GL_TRIANGLES,0, 9);
+     model.bind();
+     glDrawArrays(GL_TRIANGLES,0, 3);
+    //  glBindVertexArray(VAO);
+    //  glDrawArrays(GL_TRIANGLES,0, 3);
      ///glDrawElements(GL_TRIANGLES, 3 ,GL_UNSIGNED_INT , 0); 
     window->updateScreen();
     SDL_Delay(1000/60);
